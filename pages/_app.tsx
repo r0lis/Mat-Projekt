e27401d3817a80c83e2be236e11308a0a8e76d4d
r0lis <luky.rolenec@seznam.cz>
2023-09-24 21:58:32 +0200

    api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+
+import { ApolloProvider } from '@apollo/client';
+import type { AppProps } from 'next/app';
+import React from 'react';
+
+import { AuthContextProvider } from '@/components/AuthContextProvider';
+import { getApolloClient } from '@/utility/apollo-client';
+
+const client = getApolloClient({ forceNew: false });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </AuthContextProvider>
+  );
 }
