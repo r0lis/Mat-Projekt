dd63d94a0099210e22604742fa78817eea11ec0d
r0lis <luky.rolenec@seznam.cz>
2023-10-27 18:59:46 +0200

    update side bar team page

--- components/teamPage/SideBar.tsx
+++ components/teamPage/SideBar.tsx
@@ -9,7 +9,13 @@ import MenuIcon from '@mui/icons-material/Menu';
 import LogoTeam from '@/public/logotym.png';
 import { useRouter } from 'next/router';
 import { gql, useQuery } from '@apollo/client';
-import { Typography } from '@mui/material';
+import { Typography, CircularProgress, Avatar } from '@mui/material'; // Importujte CircularProgress z MUI
+import ChatIcon from '@mui/icons-material/Chat';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import demoUser from '@/public/demoUser.png';
+import { authUtils } from '../../firebase/auth.utils';
+
+
 
 interface SidebarProps {
     items: string[]; // seznam položek v Sidebaru
@@ -24,48 +30,87 @@ query GetTeamDetails($teamId: String!) {
 }
 `;
 
-
+const GET_USER_INFO = gql`
+  query GetUserInfo($email: String!) {
+    getUserByNameAndSurname(email: $email) {
+      Name
+      Surname
+    }
+  }
+`;
 
 
 const Sidebar: React.FC<SidebarProps> = ({ items }) => {
-
     const router = useRouter();
     const { id } = router.query;
+    const user = authUtils.getCurrentUser();
+
 
     const { loading, error, data } = useQuery(GET_TEAM_DETAILS, {
         variables: { teamId: id },
     });
 
-    if (loading) return <p>Načítání...</p>;
+    const { loading: userInfoLoading, error: userInfoError, data: userInfoData } = useQuery(GET_USER_INFO, {
+        variables: { email: user?.email || '' },
+        skip: !user,
+    });
+
+
+    if (loading) return <CircularProgress color="primary" size={50} style={{ position: 'absolute', top: '50%', left: '50%' }} />; // Zobrazí CircularProgress místo načítání
     if (error) return <p>Chyba: {error.message}</p>;
 
     const team = data.getTeamDetails;
     const teamName = team ? team.Name : '';
+    const name = userInfoData?.getUserByNameAndSurname.Name || '';
+    const surname = userInfoData?.getUserByNameAndSurname.Surname || '';
+    const initials = name[0] + surname[0];
 
     return (
         <div>
             <AppBar position="static" sx={{ backgroundColor: '#A020F0', display: 'flex', justifyContent: 'space-between', minHeight: '5em' }}>
                 <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open sidebar"
-                    >
-                        <Box sx={{marginTop:'18px'}}>
-                            <MenuIcon />
+                    <IconButton color="inherit" aria-label="open sidebar">
+                        <Box sx={{ marginTop: '18px' }}>
+                            <Link href={`/`}>
+                                <MenuIcon sx={{ color: 'white' }} />
+                            </Link>
                         </Box>
                     </IconButton>
-                    <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '25%', }}>
-                        <img src={LogoTeam.src} alt="Team Logo" style={{ width: '3.5em', height: '3.5em', marginRight: '30px',marginTop:'12px'}} />
-                       
-                            <Typography sx={{ color: 'black', fontSize: '1.7em', marginLeft: '%', marginTop:'12px' }}>{teamName}</Typography>
-
-                        
+                    <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '20%', }}>
+                        <img src={LogoTeam.src} alt="Team Logo" style={{ width: '3.5em', height: '3.5em', marginRight: '30px', marginTop: '12px' }} />
+                        <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: '1.7em', marginLeft: '%', marginTop: '12px' }}>{teamName}</Typography>
                     </Box>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '20%', }}>
+                        <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: '1.4em', marginLeft: '%', marginTop: '12px' }}>Management</Typography>
+                    </Box>
+
+                    <IconButton color="inherit" aria-label="open sidebar" sx={{ display: 'flex', marginLeft: '3%', fontSize: '24px' }}>
+                        <Box sx={{ display: 'flex', marginTop: '18px' }}>
+                            <Link href={`/`}>
+                                <ChatIcon sx={{ color: 'white' }} />
+                            </Link>
+                        </Box>
+                    </IconButton>
+
+                    <IconButton color="inherit" aria-label="open sidebar" sx={{ display: 'flex', marginLeft: '0.5%', fontSize: '24px' }}>
+                        <Box sx={{ display: 'flex', marginTop: '18px' }}>
+                            <Link href={`/`}>
+                                <CircleNotificationsIcon sx={{ color: 'white' }} />
+                            </Link>
+                        </Box>
+                    </IconButton>
+                    <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '3%', marginTop: '12px' }}>
+                        <Avatar alt="Remy Sharp" src={demoUser.src} />
+                    </Box>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '1%', }}>
+                        <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: '1.4em', marginLeft: '%', marginTop: '12px' }}>{name} {surname}</Typography>
+                    </Box>
+
                 </Toolbar>
             </AppBar>
-
             <div style={{ display: 'block', alignItems: 'center', backgroundColor: 'white', paddingTop: 'px', maxWidth: '15em', minHeight: '100%', position: 'absolute', borderRight: '4px solid lightgray', padding: '0' }}>
-
                 {items.map((item, index) => (
                     <Box
                         key={index}
@@ -75,7 +120,6 @@ const Sidebar: React.FC<SidebarProps> = ({ items }) => {
                             display: 'flex',
                             alignItems: 'center',
                             verticalAlign: 'center',
-
                         }}
                     >
                         <Link href={`/Team/${item}`} style={{ textDecoration: 'none', display: 'flex', padding: '0' }}>
@@ -84,9 +128,7 @@ const Sidebar: React.FC<SidebarProps> = ({ items }) => {
                         </Link>
                     </Box>
                 ))}
-
             </div>
-
             <div style={{ marginLeft: '15em' }}>ahoj</div>
         </div>
     );
