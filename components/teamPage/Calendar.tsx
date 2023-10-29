da5be4f99061d8324a2971e10b9066d2b5a53761
r0lis <luky.rolenec@seznam.cz>
2023-10-29 20:39:11 +0100

    demo calendar

--- components/teamPage/Calendar.tsx
+++ components/teamPage/Calendar.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const emails = ['username@gmail.com', 'user02@gmail.com']
 
 const CalendarComponent: React.FC = () => {
-  return (
-    <div>
-      <h2>cal</h2>
-    </div>
-  );
-};
+    const [calendarEvents, setCalendarEvents] = useState([
+        // Zde můžete přidat události do kalendáře ve formátu, který očekává FullCalendar
+        // Například:
+        {
+          title: 'Název události',
+          start: '2023-10-15',
+          end: '2023-10-16',
+        },
+        // Další události...
+      ]);
+    
+      // Funkce pro zachycení změn v kalendáři (přidání/úprava/odstranění události)
+      const handleEventAdd = (eventAddInfo: any) => {
+        // Zde můžete aktualizovat stav událostí v kalendáři podle akce uživatele
+        setCalendarEvents([...calendarEvents, eventAddInfo.event.toPlainObject()]);
+      };
+    
+      // Vykreslení kalendáře FullCalendar
+      return (
+        <div style={{maxWidth:'1000px'}}>
+          <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay',
+            }}
+            events={calendarEvents}
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            select={(info: any) => console.log('Selected:', info)}
+            eventAdd={handleEventAdd}
+          />
+        </div>
+      );
+    };
 
 export default CalendarComponent
