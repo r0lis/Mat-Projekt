4f2ff2150061fe85bdae1b85edfdc77c51467655
r0lis <luky.rolenec@seznam.cz>
2023-11-06 22:14:35 +0100

    FE create team step 2 a 3 style prepare

--- components/createTeam/StepperComponent/Step3.tsx
+++ components/createTeam/StepperComponent/Step3.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import { Box, Typography } from '@mui/material';
 
 const Step3: React.FC = () => {
   return (
-    <div>
-      <TextField label="Input pro krok 3" />
-      {/* Přidat další inputy nebo prvky */}
-    </div>
+    <Box sx={{ margin: '0 auto', marginTop: 10, paddingBottom: '4em' }}>
+      <Box sx={{backgroundColor: 'white', width: '60%', marginLeft: 'auto', marginRight: 'auto', padding: '5%', marginTop: '6em', borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+      <Box>
+        <Typography sx={{ textAlign: 'center' }} variant="h4" gutterBottom>Zkontrolujte údaje týmu</Typography>
+      </Box>
+      </Box>
+    </Box>
   );
 };
 
