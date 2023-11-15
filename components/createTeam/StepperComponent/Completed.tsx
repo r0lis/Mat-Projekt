ef98bdbbb35347280303e5214ef45a5dc1bfbf7f
r0lis <luky.rolenec@seznam.cz>
2023-11-16 00:20:25 +0100

    koncim

--- components/createTeam/StepperComponent/Completed.tsx
+++ components/createTeam/StepperComponent/Completed.tsx
@@ -1,40 +1,63 @@
-import { Alert, Box, Button, Typography } from '@mui/material'
+import { Alert, Box, Button, CircularProgress, Typography } from '@mui/material';
 import router from 'next/router';
-import React from 'react'
+import React, { useEffect  } from 'react';
 
-const  Completed1: React.FC = () => {
+const Completed: React.FC = () => {
 
-    const handleButtonClick = () => {
-        router.push('/');
-      };
+  const handleButtonClick = () => {
+    router.push('/');
+  };
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      router.push('/');
+    }, 4000);
 
-    return (
-        <div>
-            <Box sx={{ margin: '0 auto', marginTop: 4, }}>
-                <Box sx={{ backgroundColor: 'white', width: '60%', marginLeft: 'auto', marginRight: 'auto', padding: '5%', marginTop: '6em', borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
-                    <Typography sx={{ textAlign: 'center' }} variant="h4" gutterBottom>
-                        Hotovo
-                    </Typography>
+    return () => clearTimeout(timeoutId); // Cleanup the timeout on component unmount
+  }, []); // Empty dependency array to run the effect only once
 
-                    <Box sx={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
+  return (
+    <div>
+      <Box sx={{ margin: '0 auto', marginTop: 4 }}>
+        <Box
+          sx={{
+            backgroundColor: 'white',
+            width: '60%',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            padding: '5%',
+            marginTop: '6em',
+            borderRadius: '10px',
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          <Typography sx={{ textAlign: 'center' }} variant="h4" gutterBottom>
+            Hotovo
+          </Typography>
 
-                        <Alert severity="success">
-                            Tým byl úspěšně vytvořen!
-                        </Alert>
-                    </Box>
-                    <Button
-              variant="contained"
-              color="primary"
-              onClick={handleButtonClick}
-              sx={{ marginTop: '2em' }}
-            >
-              Zpět na úvodní stránku
-            </Button>
-                </Box>
+          
+            <Box sx={{ textAlign: 'center', marginTop: '2em' }}>
+              <CircularProgress />
             </Box>
-        </div>
-    )
+         
+          
+              <Box sx={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
+                <Alert severity="success">Tým byl úspěšně vytvořen! Jste přesunuti na hlavni stranku</Alert>
+              </Box>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleButtonClick}
+                sx={{ marginTop: '2em' }}
+              >
+                Zpět na úvodní stránku
+              </Button>
+            
+         
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
-export default Completed1
+export default Completed;
