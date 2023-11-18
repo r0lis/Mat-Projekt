fc372cd7c3cffb0c309a03c724d545533c73a0b5
r0lis <luky.rolenec@seznam.cz>
2023-11-18 13:34:34 +0100

    email sending

--- components/createTeam/StepperComponent/Completed.tsx
+++ components/createTeam/StepperComponent/Completed.tsx
@@ -1,8 +1,17 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { gql, useQuery } from '@apollo/client';
-import { Alert, Box, Button, CircularProgress, Typography } from '@mui/material';
-import React, { useEffect  } from 'react';
+import { gql, useQuery } from "@apollo/client";
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Typography,
+} from "@mui/material";
+import React, { useEffect } from "react";
+import axios from "axios";
+
+
 
 type CompletedProps = {
   teamEmail: string;
@@ -14,20 +23,23 @@ const GET_TEAM_MEMBERS = gql`
   }
 `;
 
-const Completed: React.FC<CompletedProps> = ({teamEmail}) => {
-
+const Completed: React.FC<CompletedProps> = ({ teamEmail }) => {
   const { loading, error, data } = useQuery(GET_TEAM_MEMBERS, {
     variables: { teamEmail },
   });
 
-  const handleButtonClick = () => {
-    
-  };
+  const handleButtonClick = async () => {
+    const members = data?.getTeamMembersByEmail || [];
 
+    try {
+      await axios.post('/api/sendEmail', { emails: members });
+      console.log('E-maily úspěšně odeslány.');
+    } catch (error) {
+      console.error('Chyba při odesílání e-mailů:', error);
+    }
+  };
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      
-    }, 4000);
+    const timeoutId = setTimeout(() => {}, 4000);
 
     return () => clearTimeout(timeoutId); // Cleanup the timeout on component unmount
   }, []); // Empty dependency array to run the effect only once
@@ -35,68 +47,60 @@ const Completed: React.FC<CompletedProps> = ({teamEmail}) => {
   const members = data?.getTeamMembersByEmail || [];
 
   if (loading) {
-    return   <CircularProgress />
-    ;
+    return <CircularProgress />;
   }
 
   if (error) {
     return <p>Error: {error.message}</p>;
   }
 
-
   return (
     <div>
-      <Box sx={{ margin: '0 auto', marginTop: 4 }}>
+      <Box sx={{ margin: "0 auto", marginTop: 4 }}>
         <Box
           sx={{
-            backgroundColor: 'white',
-            width: '60%',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            padding: '5%',
-            marginTop: '6em',
-            borderRadius: '10px',
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+            backgroundColor: "white",
+            width: "60%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            padding: "5%",
+            marginTop: "6em",
+            borderRadius: "10px",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
           }}
         >
-          <Typography sx={{ textAlign: 'center' }} variant="h4" gutterBottom>
-            {teamEmail}
-          </Typography>
-
-          {members.length > 0 ? (
-          <div>
-            <Typography variant="h6">Team Members:</Typography>
-            <ul>
-              {members.map((member, index) => (
-                <li key={index}>{member}</li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <Typography variant="body1">No team members found.</Typography>
-        )}
-         
-         
           
-              <Box sx={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
-                <Alert severity="success">Tým byl úspěšně vytvořen! Jste přesunuti na hlavni stranku</Alert>
-              </Box>
+          {members.length > 0 ? (
+            <div>
+              <Typography variant="h6">Team Members:</Typography>
+              <ul>
+                {members.map((member: string, index: number) => (
+                  <li key={index}>{member}</li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <Typography variant="body1">No team members found.</Typography>
+          )}
 
-              
-            <Box sx={{ textAlign: 'center', marginTop: '2em' }}>
-              <CircularProgress />
-            </Box>
+          <Box sx={{ width: "50%", marginLeft: "auto", marginRight: "auto" }}>
+            <Alert severity="success">
+              Tým byl úspěšně vytvořen! Jste přesunuti na hlavni stranku
+            </Alert>
+          </Box>
 
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleButtonClick}
-                sx={{ marginTop: '2em' }}
-              >
-                Zpět na úvodní stránku
-              </Button>
-            
-         
+          <Box sx={{ textAlign: "center", marginTop: "2em" }}>
+            <CircularProgress />
+          </Box>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleButtonClick}
+            sx={{ marginTop: "2em" }}
+          >
+            Zpět na úvodní stránku
+          </Button>
         </Box>
       </Box>
     </div>
---
+++ pages/api/sendEmail.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+// pages/api/sendEmail.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { emails } = req.body;
+
+  if (!emails || !Array.isArray(emails) || emails.length === 0) {
+    return res.status(400).json({ error: 'Invalid or empty email list.' });
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'lukyrolenec@gmail.com', // Nahraďte svou e-mailovou adresou
+      pass: 'paqq oaxc fkhe ylba', // Nahraďte heslem své e-mailové adresy
+
+    },
+  });
+
+  const mailOptions = {
+    from: 'your.email@gmail.com', // Nahraďte svou e-mailovou adresou
+    subject: 'Pozvánka na registraci',
+    text: 'Registrace: http://localhost/UserRegistration', // Nahraďte URL své aplikace
+  };
+
+  try {
+    for (const email of emails) {
+      const optionsWithEmail = { ...mailOptions, to: email };
+      await transporter.sendMail(optionsWithEmail);
+      console.log(`E-mail úspěšně odeslán na ${email}`);
+    }
+
+    res.status(200).json({ message: 'E-maily úspěšně odeslány.' });
+  } catch (error) {
+    console.error('Chyba při odesílání e-mailů:', error);
+    res.status(500).json({ error: 'Chyba při odesílání e-mailů.' });
+  }
+}
