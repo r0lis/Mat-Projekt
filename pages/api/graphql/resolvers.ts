ce94a9b326f1085412d6ba127b2af0f73c0fd00a
r0lis <luky.rolenec@seznam.cz>
2023-12-16 10:16:20 +0100

    [Cleanup] resolvers

--- pages/api/graphql/resolvers.ts
+++ pages/api/graphql/resolvers.ts
@@ -2,23 +2,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/ban-types */
-import { firestore } from "firebase-admin";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
-import {
-  Context,
-  User,
-  Team,
-  UpdatedMemberInput,
-  CreateUserInput,
-  CreateTeamInput,
-  NameAndSurname,
-  TeamDetails,
-  Query,
-  CreateUserToTeamInput,
-  TeamDetails2,
-} from "./types";
 import 'firebase/storage';
 import { userQueries } from "./resolvers/queries/userQueries";
 import { teamQueries } from "./resolvers/queries/teamQueries";
@@ -26,7 +9,6 @@ import { userMutations } from "./resolvers/mutations/userMutations";
 import { teamMutations } from "./resolvers/mutations/teamMutations";
 import {subteamMutations} from "./resolvers/mutations/subteamMutations";
 import {subteamQueries} from "./resolvers/queries/subteamQueries";
-import * as admin from 'firebase-admin';
 
 export const resolvers = {
   Query: {
