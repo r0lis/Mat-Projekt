90c52071d26134d99da5a6492621471fef9b0e9e
r0lis <luky.rolenec@seznam.cz>
2024-02-15 22:30:52 +0100

    [Feature] update  rousters

--- components/teamPage/Rousters/ContentRouster.tsx
+++ components/teamPage/Rousters/ContentRouster.tsx
@@ -17,6 +17,8 @@ import {
   Typography,
   TextField,
   Autocomplete,
+  Button,
+  Modal,
 } from "@mui/material";
 
 const GET_COMPLETESUBTEAM_DETAILS = gql`
@@ -94,8 +96,10 @@ const ContentRouster: React.FC<ContentRousterProps> = ({
     position: string;
 
   } | null>(null);
-  const [selectedRole, setSelectedRole] = useState("");
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [, setSelectedRole] = useState("");
+  const [, setSelectedIndex] = useState<number | null>(null);
+  const [editMode, setEditMode] = useState(false);
+  const currentUserEmail = user?.email || "";
   const [modalOpen, setModalOpen] = useState(false);
 
 
@@ -159,7 +163,6 @@ const ContentRouster: React.FC<ContentRousterProps> = ({
     setSelectedRole(member.role);
     setModalOpen(true);
 
-    // Update the selected index based on the filtered list
     setSelectedIndex(calculateSelectedIndex());
   };
 
@@ -173,6 +176,29 @@ const ContentRouster: React.FC<ContentRousterProps> = ({
     }
     return null;
   };
+
+  const handleCloseModal = () => {
+    setSelectedMember(null);
+    setModalOpen(false);
+    setEditMode(false);
+  };
+
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const calculateAge = (dateOfBirth: string) => {
+    const dob = new Date(dateOfBirth);
+    const today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
 
   return (
     <Box>
@@ -270,6 +296,448 @@ const ContentRouster: React.FC<ContentRousterProps> = ({
           </TableBody>
         </Table>
       </TableContainer>
+      <Modal
+        open={modalOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 4,
+            borderRadius: "8px",
+            width: 400,
+            backgroundImage: editMode
+              ? `
+            linear-gradient(to bottom, #808080 0, #909090 100px, #ffffff 100px, #ffffff calc(100% - 100px), #909090 calc(100% - 100px), #909090 100%)
+          `
+              : selectedMember && selectedMember.email == currentUserEmail
+              ? `
+            linear-gradient(to bottom, #808080 0, #909090 100px, #ffffff 100px, #ffffff calc(100% - 110px), #909090 calc(100% - 110px), #909090 100%)
+          `
+              : `
+          linear-gradient(to bottom, #808080 0, #909090 100px, #ffffff 100px, #ffffff calc(100% - 140px), #909090 calc(100% - 140px), #909090 100%)
+        `,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                fontFamily: "Roboto",
+                fontWeight: "500",
+                opacity: "0.6",
+                marginLeft: "1em",
+                top: "-10px",
+                position: "relative",
+              }}
+            >
+              člen
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              padding: "0em 0em 0em 1em",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box
+              sx={{ marginBottom: "1em", top: "-10px", position: "relative" }}
+            >
+              <Typography id="modal-title" variant="h6" component="h2">
+                {selectedMember?.name} {selectedMember?.surname}
+              </Typography>
+            </Box>
+            <Avatar
+              sx={{
+                width: 60,
+                height: 60,
+                marginLeft: "0.5em",
+                top: "-30px",
+                right: "1em",
+              }}
+              alt="Remy Sharp"
+              src={selectedMember?.picture}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box sx={{ padding: "0em 2em 0em 2em" }}>
+              <Typography
+                id="modal-description"
+                sx={{
+                  marginTop: "1em",
+                  fontFamily: "Roboto",
+                  fontSize: "1em",
+                  marginBottom: "1em",
+                  color: "black",
+                  fontWeight: "500",
+                }}
+              >
+                Věk:
+              </Typography>
+              <Typography
+                id="modal-description"
+                sx={{
+                  marginTop: "1em",
+                  fontFamily: "Roboto",
+                  fontSize: "1em",
+                  marginBottom: "1em",
+                  whiteSpace: "nowrap",
+                  color: "black",
+                  fontWeight: "500",
+                }}
+              >
+                Datum narození:
+              </Typography>
+              
+              <Typography
+                id="modal-description"
+                sx={{
+                  marginTop: "1em",
+                  fontFamily: "Roboto",
+                  fontSize: "1em",
+                  marginBottom: "1em",
+                  color: "black",
+                  fontWeight: "500",
+                }}
+              >
+                Email:
+              </Typography>
+              <Typography
+                id="modal-description"
+                sx={{
+                  marginTop: "1em",
+                  fontFamily: "Roboto",
+                  fontSize: "1em",
+                  marginBottom: "1em",
+                  whiteSpace: "nowrap",
+                  color: "black",
+                  fontWeight: "500",
+                }}
+              >
+                Zdravotní prohlídka:
+              </Typography>
+            </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                marginRight: "2em",
+              }}
+            >
+              <Typography
+                id="modal-description"
+                sx={{
+                  marginTop: "1em",
+                  marginBottom: "1em",
+                  color: "black",
+                  fontWeight: "500",
+                }}
+              >
+                {calculateAge(selectedMember?.dateOfBirth || "")} let
+              </Typography>
+
+              <Typography
+                id="modal-description"
+                sx={{ marginBottom: "1em", color: "black", fontWeight: "500" }}
+              >
+                {selectedMember?.dateOfBirth &&
+                  new Date(selectedMember.dateOfBirth).toLocaleDateString(
+                    "cs-CZ",
+                    {
+                      day: "2-digit",
+                      month: "2-digit",
+                      year: "numeric",
+                    }
+                  )}
+              </Typography>
+              <Typography
+                id="modal-description"
+                sx={{
+                  marginBottom: "1em",
+                  color: "black",
+                  fontWeight: "500",
+                  whiteSpace: "nowrap",
+                }}
+              >
+                {selectedMember?.email}
+              </Typography>
+
+              <Typography
+                id="modal-description"
+                sx={{
+                  marginBottom: "1em",
+                  whiteSpace: "nowrap",
+                  color: "black",
+                  fontWeight: "500",
+                }}
+              >
+                do 2. 3. 2023
+              </Typography>
+
+              {editMode ? (
+                <Box sx={{}}>
+                  Edit
+                </Box>
+              ) : (
+               <Box sx={{}}>
+                </Box>
+              )}
+            </Box>
+          </Box>
+
+          {editMode ? (
+            <Box sx={{ paddingLeft: "1em" }}>
+              <Box
+                sx={{
+                  fontFamily: "Roboto",
+                  fontWeight: "500",
+                  opacity: "0.6",
+
+                  top: "-10px",
+                  position: "relative",
+                  marginTop: "1em",
+                }}
+              >
+                štítky
+              </Box>
+
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box
+                  sx={{
+                    border: "1px solid black",
+                    textAlign: "center",
+                    padding: "3px",
+                    borderRadius: "8px",
+                    width: "6em",
+                  }}
+                >
+                  <Typography sx={{ fontFamily: "Roboto", fontWeight: "500" }}>
+                    útočník
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    border: "1px solid black",
+                    textAlign: "center",
+                    padding: "3px",
+                    borderRadius: "8px",
+                    width: "6em",
+                  }}
+                >
+                  <Typography sx={{ fontFamily: "Roboto", fontWeight: "500" }}>
+                    A-Tým
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    border: "1px solid black",
+                    textAlign: "center",
+                    padding: "3px",
+                    borderRadius: "8px",
+                    width: "6em",
+                  }}
+                >
+                  <Typography sx={{ fontFamily: "Roboto", fontWeight: "500" }}>
+                    Levá strana
+                  </Typography>
+                </Box>
+              </Box>
+              <Button
+                sx={{
+                  backgroundColor: "lightgray",
+                  color: "black",
+                  marginTop: "4em",
+                  padding: "1em",
+                  height: "2.5em",
+                  marginLeft: "-0.5em",
+                  width: "100%",
+                }}
+              >
+                Uložit
+              </Button>
+            </Box>
+          ) : (
+            <Box sx={{ paddingLeft: "1em" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  sx={{
+                    width: "100%",
+                    borderBottom: "2px solid gray", // Change the color as needed
+                    position: "absolute",
+                    top:
+                      selectedMember?.role === "0" ||
+                      selectedMember?.role === "No Role Assigned"
+                        ? "55%"
+                        : selectedMember &&
+                          selectedMember.email === currentUserEmail
+                        ? "59%"
+                        : "55%",
+                    transform: "translateY(-50%)",
+                    marginLeft: "-3em",
+                    zIndex: -1,
+                  }}
+                />
+                <Box
+                  sx={{
+                    fontFamily: "Roboto",
+                    fontWeight: "500",
+                    opacity: "0.6",
+                    marginLeft: "-0.5em",
+                    top: "-10px",
+                    position: "relative",
+                    marginTop: "1em",
+                  }}
+                >
+                  štítky
+                </Box>
+              </Box>
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <Box
+                  sx={{
+                    border: "1px solid black",
+                    textAlign: "center",
+                    padding: "3px",
+                    borderRadius: "8px",
+                    width: "6em",
+                  }}
+                >
+                  <Typography sx={{ fontFamily: "Roboto", fontWeight: "500" }}>
+                    útočník
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    border: "1px solid black",
+                    textAlign: "center",
+                    padding: "3px",
+                    borderRadius: "8px",
+                    width: "6em",
+                  }}
+                >
+                  <Typography sx={{ fontFamily: "Roboto", fontWeight: "500" }}>
+                    A-Tým
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    border: "1px solid black",
+                    textAlign: "center",
+                    padding: "3px",
+                    borderRadius: "8px",
+                    width: "6em",
+                  }}
+                >
+                  <Typography sx={{ fontFamily: "Roboto", fontWeight: "500" }}>
+                    Levá strana
+                  </Typography>
+                </Box>
+              </Box>
+
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  sx={{
+                    width: "100%",
+                    borderBottom: "2px solid gray", // Change the color as needed
+                    position: "absolute",
+                    top:
+                      selectedMember?.role === "0" ||
+                      selectedMember?.role === "No Role Assigned"
+                        ? "81%"
+                        : selectedMember &&
+                          selectedMember.email === currentUserEmail
+                        ? "68%"
+                        : "72.2%",
+                    transform: "translateY(-50%)",
+                    marginLeft: "-3em",
+                    zIndex: -1,
+                  }}
+                />
+                
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Box
+                  sx={{
+                    fontFamily: "Roboto",
+                    fontWeight: "500",
+                    opacity: "0.6",
+                    marginLeft: "-0.5em",
+                    top: "-10px",
+                    position: "relative",
+                    marginTop: "2em",
+                  }}
+                >
+                  Management
+                </Box>
+              </Box>
+
+              <Button
+                sx={{
+                  backgroundColor: "lightgray",
+                  color: "black",
+                  padding: "1em",
+                  height: "2em",
+                  marginLeft: "-0.5em",
+                  marginTop: "0.5em",
+                  width: "100%",
+                }}
+                onClick={handleEditClick}
+              >
+                Upravit
+              </Button>
+              {selectedMember && selectedMember.email !== currentUserEmail && (
+                <Button
+                  sx={{
+                    backgroundColor: "lightgray",
+                    color: "black",
+                    padding: "1em",
+                    height: "2em",
+                    marginLeft: "-0.5em",
+                    marginTop: "0.5em",
+                    width: "100%",
+                  }}
+                >
+                  Smazat
+                </Button>
+              )}
+            </Box>
+          )}
+        </Box>
+      </Modal>
     </Box>
   );
 };
