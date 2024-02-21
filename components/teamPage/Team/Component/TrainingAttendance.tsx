1c3f2e9914c57dc030fa4464acaecba7e9ad9773
r0lis <luky.rolenec@seznam.cz>
2024-02-21 23:19:16 +0100

    [Feature] update attandace graphs and services

--- components/teamPage/Team/Component/TrainingAttendance.tsx
+++ components/teamPage/Team/Component/TrainingAttendance.tsx
@@ -14,9 +14,24 @@ import {
   Select,
   MenuItem,
 } from "@mui/material";
-import { CheckCircle as CheckCircleIcon, Cancel as CancelIcon, Help as HelpIcon } from "@mui/icons-material";
+import {
+  CheckCircle as CheckCircleIcon,
+  Cancel as CancelIcon,
+  Help as HelpIcon,
+} from "@mui/icons-material";
 import { authUtils } from "@/firebase/auth.utils";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, PieChart, Pie, Cell } from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  PieChart,
+  Pie,
+  Cell,
+} from "recharts";
 
 const GET_ALL_MATCHES_BY_SUBTEAM_ID = gql`
   query GetAllTrainingBySubteamId($subteamId: String!) {
@@ -99,15 +114,18 @@ const TrainingAttendance: React.FC<{ subteamId: string }> = ({ subteamId }) => {
   const matches = matchData.getAllTrainingBySubteamId;
   const subteamDetail = subteamData.getCompleteSubteamDetail;
 
-  const currentUserMember = subteamDetail?.subteamMembers.find((member: { email: string | null | undefined; }) => member.email === CurrentUserEmail);
+  const currentUserMember = subteamDetail?.subteamMembers.find(
+    (member: { email: string | null | undefined }) =>
+      member.email === CurrentUserEmail
+  );
 
-  let filteredMembers: { email: string, name: string, surname: string }[] = [];
-  if (currentUserMember && currentUserMember.role === '3') {
+  let filteredMembers: { email: string; name: string; surname: string }[] = [];
+  if (currentUserMember && currentUserMember.role === "3") {
     filteredMembers = [currentUserMember];
   } else {
     filteredMembers =
       subteamDetail?.subteamMembers.filter(
-        (        member: { role: string; }) => member.role === "3"
+        (member: { role: string }) => member.role === "3"
       ) || [];
   }
 
@@ -115,7 +133,7 @@ const TrainingAttendance: React.FC<{ subteamId: string }> = ({ subteamId }) => {
     (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
   );
 
-  const filteredMatches = sortedMatches.filter(match => {
+  const filteredMatches = sortedMatches.filter((match) => {
     if (filterType === "all") return true;
 
     const matchDate = new Date(match.date);
@@ -134,14 +152,31 @@ const TrainingAttendance: React.FC<{ subteamId: string }> = ({ subteamId }) => {
     return true;
   });
 
-  const selectedMemberAttendance = selectedMember ? filteredMatches.map(match => {
-    const attendance = match.attendance?.find((a: { player: string; }) => a.player === selectedMember);
-    return { name: match.date, hisAttendance: attendance ? attendance.hisAttendance : 0 };
-  }) : [];
+  const selectedMemberAttendance = selectedMember
+    ? filteredMatches.map((match) => {
+        const attendance = match.attendance?.find(
+          (a: { player: string }) => a.player === selectedMember
+        );
+        return {
+          name: match.date,
+          hisAttendance: attendance ? attendance.hisAttendance : 0,
+        };
+      })
+    : [];
 
   const pieChartData = [
-    { name: 'Presence', value: selectedMemberAttendance.filter(entry => entry.hisAttendance === 1).length },
-    { name: 'Absence', value: selectedMemberAttendance.filter(entry => entry.hisAttendance === 2).length }
+    {
+      name: "Přítomen",
+      value: selectedMemberAttendance.filter(
+        (entry) => entry.hisAttendance === 1
+      ).length,
+    },
+    {
+      name: "Nepřítomen",
+      value: selectedMemberAttendance.filter(
+        (entry) => entry.hisAttendance === 2
+      ).length,
+    },
   ];
 
   return (
@@ -172,7 +207,7 @@ const TrainingAttendance: React.FC<{ subteamId: string }> = ({ subteamId }) => {
               <TableHead>
                 <TableRow>
                   <TableCell>Člen</TableCell>
-                  {filteredMatches.map(match => (
+                  {filteredMatches.map((match) => (
                     <TableCell key={match.matchId}>
                       {match.date &&
                         new Date(match.date).toLocaleDateString("cs-CZ", {
@@ -185,13 +220,18 @@ const TrainingAttendance: React.FC<{ subteamId: string }> = ({ subteamId }) => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {filteredMembers.map(member => (
+                {filteredMembers.map((member) => (
                   <TableRow key={member.email}>
-                    <TableCell onClick={() => setSelectedMember(member.email)} style={{ cursor: 'pointer' }}>
+                    <TableCell
+                      onClick={() => setSelectedMember(member.email)}
+                      style={{ cursor: "pointer" }}
+                    >
                       {member.name} {member.surname}
                     </TableCell>
-                    {filteredMatches.map(match => {
-                      const attendance = match.attendance?.find((a: { player: string; }) => a.player === member.email);
+                    {filteredMatches.map((match) => {
+                      const attendance = match.attendance?.find(
+                        (a: { player: string }) => a.player === member.email
+                      );
 
                       return (
                         <TableCell key={match.matchId}>
@@ -201,7 +241,9 @@ const TrainingAttendance: React.FC<{ subteamId: string }> = ({ subteamId }) => {
                             ) : attendance.hisAttendance === 2 ? (
                               <CancelIcon sx={{ color: "red" }} />
                             ) : (
-                              <HelpIcon sx={{ color: "rgba(200, 198, 199, 0.8)" }} />
+                              <HelpIcon
+                                sx={{ color: "rgba(200, 198, 199, 0.8)" }}
+                              />
                             )
                           ) : (
                             "-"
@@ -210,12 +252,17 @@ const TrainingAttendance: React.FC<{ subteamId: string }> = ({ subteamId }) => {
                       );
                     })}
                     <TableCell>
-                      {`${Math.round(calculateAttendancePercentage(
-                        filteredMatches.map(match => {
-                          const attendance = match.attendance?.find((a: { player: string; }) => a.player === member.email);
-                          return attendance ? attendance.hisAttendance : 0;
-                        })
-                      ))} / 100`}
+                      {`${Math.round(
+                        calculateAttendancePercentage(
+                          filteredMatches.map((match) => {
+                            const attendance = match.attendance?.find(
+                              (a: { player: string }) =>
+                                a.player === member.email
+                            );
+                            return attendance ? attendance.hisAttendance : 0;
+                          })
+                        )
+                      )} / 100`}
                     </TableCell>
                   </TableRow>
                 ))}
@@ -223,24 +270,84 @@ const TrainingAttendance: React.FC<{ subteamId: string }> = ({ subteamId }) => {
             </Table>
           </TableContainer>
           {selectedMember && (
-            <Box mt={4}>
-              <Typography variant="h6">Attendance of {selectedMember}</Typography>
-              <LineChart width={800} height={400} data={selectedMemberAttendance}>
+            <Box mt={4} sx={{marginLeft:"1em"}}>
+              <Typography variant="h6">
+    Docházka hráče:{" "}
+    {selectedMember &&
+      filteredMembers.find(
+        (member) => member.email === selectedMember
+      )?.name}{" "}
+    {selectedMember &&
+      filteredMembers.find(
+        (member) => member.email === selectedMember
+      )?.surname}
+  </Typography>
+              <LineChart
+                width={900}
+                height={400}
+                data={selectedMemberAttendance}
+              >
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
+                <XAxis  dataKey="name" />
+                <YAxis
+                  tick={{ fontSize: 14 }}
+                  tickFormatter={(value) =>
+                    value === 1
+                      ? "Ano"
+                      : value === 2
+                      ? "Ne"
+                      : value === 0
+                      ?"NZ"
+                      : ""
+                  }
+                />{" "}
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="hisAttendance" stroke="#8884d8" />
+                <Line
+                  type="monotone"
+                  dataKey="hisAttendance"
+                  stroke="#8884d8"
+                />
               </LineChart>
-              <PieChart width={400} height={400}>
-                <Pie data={pieChartData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={80} fill="#8884d8">
+              <Box display="flex" alignItems="center" justifyContent="center">
+                <PieChart width={400} height={400}>
+                  <Pie
+                    data={pieChartData}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={80}
+                    fill="#8884d8"
+                    label={({ name, value }) =>
+                      `${name}: ${(
+                        (value / selectedMemberAttendance.length) *
+                        100
+                      ).toFixed(2)}%`
+                    }
+                  >
+                    {pieChartData.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={entry.name === "Nepřítomen" ? "red" : "green"}
+                      />
+                    ))}
+                  </Pie>
+                  <Tooltip />
+                </PieChart>
+                <Box ml={5}>
                   {pieChartData.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={entry.value === 2 ? 'red' : 'green'} />
+                    <Typography key={index}>
+                      {entry.name}:{" "}
+                      {(
+                        (entry.value / selectedMemberAttendance.length) *
+                        100
+                      ).toFixed(2)}
+                      %
+                    </Typography>
                   ))}
-                </Pie>
-                <Tooltip />
-              </PieChart>
+                </Box>
+              </Box>
             </Box>
           )}
         </>
