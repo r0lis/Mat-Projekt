4628b0440f30f6f70d9a1041d84117a38795b452
r0lis <luky.rolenec@seznam.cz>
2024-03-04 20:37:41 +0100

    [Fe] update fix and style

--- pages/LoginPage.tsx
+++ pages/LoginPage.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import React, { useState } from 'react';
-import { authUtils } from '../firebase/auth.utils';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { Box, Button, TextField, Typography } from '@mui/material';
+import React, { useState } from "react";
+import { authUtils } from "../firebase/auth.utils";
+import { useRouter } from "next/router";
+import { Box, Button, TextField, Typography, Link } from "@mui/material";
+import photo from "../public/assets/rosterbot.png";
+import pictureBackground from "../public/assets/uvodni.jpg";
 
 const LoginPage: React.FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -20,11 +21,10 @@ const LoginPage: React.FC = () => {
       setIsLoggedIn(true);
       setError(null);
 
-
-      router.push('/');
+      router.push("/");
     } catch (error) {
-      console.error('Chyba při přihlašování:', error);
-      setError('Přihlášení selhalo. Zkontrolujte e-mail a heslo.');
+      console.error("Chyba při přihlašování:", error);
+      setError("Přihlášení selhalo. Zkontrolujte e-mail a heslo.");
       setIsLoggedIn(false);
     }
   };
@@ -32,58 +32,230 @@ const LoginPage: React.FC = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: '40%',
-        margin: '0 auto',
+        backgroundColor: "#F0F2F5",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Typography variant="h4">Přihlášení</Typography>
+      <Box
+        sx={{
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "80%",
+          boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.4)",
+          borderRadius: "15px",
+          margin: "0 1rem",
+          position: "relative",
+        }}
+      >
+        <Box sx={{ display: "flex", flexDirection: "row", width: "100%" }}>
+          <Box
+            sx={{
+              width: "90%",
+              position: "relative",
+              zIndex: "1", // Ensure content is above the background image
+              borderRadius: "0 0 15px 15px",
+            }}
+          >
+            <img
+              src={pictureBackground.src}
+              alt=""
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                opacity: 0.85,
+                borderRadius: "15px 0 0px 15px",
+              }}
+            />
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                zIndex: "2", // Set a higher z-index for content
+              }}
+            >
+              <Box
+                sx={{ marginLeft: "10%", marginRight: "10%", zIndex: "999" }}
+              >
+                <Typography
+                  sx={{
+                    color: "white",
+                    fontFamily: "Roboto",
+                    fontWeight: "700",
+                    marginTop: "1em",
+                  }}
+                >
+                  LOGO
+                </Typography>
+                <Typography
+                  variant="h4"
+                  sx={{
+                    margin: "1rem",
+                    marginTop: "0.7em",
+                    marginBottom: "auto",
+                    fontSize: "4vw",
+                    fontFamily: "Roboto",
+                    fontWeight: "bold",
+                    color: "white",
+                    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+                  }}
+                >
+                  TEAM MANAGER
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  marginLeft: "10%",
+                  marginRight: "10%",
+                  zIndex: "999",
+                  marginTop: "2em",
+                  position: "relative",
+                }}
+              >
+                <img src={photo.src} alt="logo" width="100%" height="auto" />
+              </Box>
+            </Box>
+          </Box>
+          <Box sx={{ width: "60%" }}>
+            <Box
+              sx={{
+                width: "75%", // Set the desired width for the box
+                mx: "auto",
+              }}
+            >
+              <Box
+                sx={{
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  marginTop: "2em",
+                  textAlign: "center",
+                }}
+              >
+                <Typography
+                  variant="h4"
+                  sx={{
+                    margin: "1rem",
+                    marginTop: "1em",
+                    fontFamily: "Roboto",
+                    fontWeight: "500",
+                  }}
+                >
+                  Přihlášení
+                </Typography>
+              </Box>
 
-      <TextField
-        type="email"
-        label="E-mail"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        fullWidth
-        margin="normal"
-      />
-      <TextField
-        type="password"
-        label="Heslo"
-        value={password}
-        onSubmit={handleLogin}
-        onChange={(e) => setPassword(e.target.value)}
-        fullWidth
-        margin="normal"
-        
-      />
-      <Button variant="contained" color="primary" onClick={handleLogin} sx={{ marginTop: '1rem' }}>
-        Přihlásit
-      </Button>
-      <Box sx={{ marginTop: '1rem' }}>
-        <Link href="/UserRegistration">
-          Registrovat
-        </Link>
+              <Box>
+                <TextField
+                  type="email"
+                  label="E-mail"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  type="password"
+                  label="Heslo"
+                  value={password}
+                  onSubmit={handleLogin}
+                  onChange={(e) => setPassword(e.target.value)}
+                  fullWidth
+                  margin="normal"
+                />
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleLogin}
+                  sx={{
+                    marginTop: "1rem",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    display: "block",
+                    backgroundColor: "#FFE0FE",
+                    color: "black",
+                    fontFamily: "Roboto",
+                    fontWeight: "700",
+                    border: "1px solid #ff96fc",
+                    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+                    padding: "0.7em",
+                    borderRadius: "4px",
+                  }}
+                >
+                  Přihlásit
+                </Button>
+                <Box
+                  sx={{
+                    marginTop: "1em",
+                    marginBottom: "2em",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    display: "block",
+                    textAlign: "center",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      fontFamily: "Roboto",
+                      fontWeight: "700",
+                      color: "#b71dde",
+                    }}
+                  >
+                    <Link
+                      href="/UserRegistration"
+                      sx={{
+                        marginRight: "1rem",
+                        marginTop: "1rem",
+                        color: "#b71dde",
+                        textDecoration: "none",
+                      }}
+                    >
+                      Registrovat
+                    </Link>
+                    <Link
+                      href="/"
+                      sx={{
+                        marginTop: "1rem",
+                        color: "#b71dde",
+                        textDecoration: "none",
+                      }}
+                    >
+                      Zpět
+                    </Link>
+                  </Typography>
+                </Box>
+
+                {error && (
+                  <Typography
+                    variant="body1"
+                    color="error"
+                    sx={{ marginTop: "1rem" }}
+                  >
+                    {error}
+                  </Typography>
+                )}
+                {isLoggedIn && (
+                  <Typography
+                    variant="body1"
+                    color="success"
+                    sx={{ marginTop: "1rem" }}
+                  >
+                    Přihlášení úspěšné.
+                  </Typography>
+                )}
+              </Box>
+            </Box>
+          </Box>
+        </Box>
       </Box>
-      <Box sx={{ marginTop: '1rem' }}>
-        <Link href="/">
-          Zpět
-        </Link>
-      </Box>
-
-
-      {error && (
-        <Typography variant="body1" color="error" sx={{ marginTop: '1rem' }}>
-          {error}
-        </Typography>
-      )}
-      {isLoggedIn && (
-        <Typography variant="body1" color="success" sx={{ marginTop: '1rem' }}>
-          Přihlášení úspěšné.
-        </Typography>
-      )}
     </Box>
   );
 };
